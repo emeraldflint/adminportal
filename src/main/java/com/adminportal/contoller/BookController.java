6cad529f5f580205ea57893680cb02dda59dd211
EugeneKasap <Evgeniy.Kasap@diymaxeda.com>
2017-05-31 23:54:00 +0300

    Added View for BookList.

--- src/main/java/com/adminportal/contoller/BookController.java
+++ src/main/java/com/adminportal/contoller/BookController.java
@@ -51,7 +51,10 @@ public class BookController {
 
     @RequestMapping("/bookList")
     public String bookList(Model model) {
-        //List<Book> bookList = bookService.findAll();
+        List<Book> bookList = bookService.findAll();
+        model.addAttribute("bookList", bookList);
+
+
         return "bookList";
     }
 }
--- src/main/java/com/adminportal/service/BookService.java
+++ src/main/java/com/adminportal/service/BookService.java
@@ -2,6 +2,9 @@ package com.adminportal.service;
 
 import com.adminportal.domain.Book;
 
+import java.util.List;
+
 public interface BookService {
     Book save(Book book);
+    List<Book> findAll();
 }
